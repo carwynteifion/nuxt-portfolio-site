cc31c9ca1cd611e2d4e84a3094d10e1cd449c068
Carwyn Thomas <hello@carwyncodes.dev>
2024-11-02 01:05:56 +0000

    Add first project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['nuxt-icon'],
+  modules: ['@nuxt/icon'],
   ssr: false,
+  compatibilityDate: '2024-11-02',
 });
--- pages/projects.vue
+++ pages/projects.vue
@@ -26,5 +26,24 @@ definePageMeta({
         <p>This website! Click the title to view the source code.</p>
       </div>
     </div>
+    <div class="white-curved-border">
+      <div id="chrysalis">
+        <h3>
+          <NuxtLink
+            to="https://chrysalisgroupservices.co.uk"
+            target="_blank"
+            external
+          >
+            Chrysalis Group Services
+          </NuxtLink>
+        </h3>
+        <p>
+          My first project! I built a user-centric site for a Bristol-based
+          housing association charity, focused on housing people with additional
+          or complex needs.
+        </p>
+        <p>Tools: HTML5, CSS3, Bootstrap 5</p>
+      </div>
+    </div>
   </div>
 </template>
